08b024248e95f9e34abdd72c373a41f3568aa518
ugolyokk <vorbidrug@gmail.com>
2022-05-19 21:08:12 +0300

    add task3

--- python/task2.py
+++ python/task2.py
@@ -2,11 +2,10 @@
 
 import os
 
-bash_command = ["cd ~/netology/sysadm-homeworks", "git status"]
+bash_command = ["cd ~/netology/devops-netology/python", "git status"]
 result_os = os.popen(' && '.join(bash_command)).read()
 is_change = False
 for result in result_os.split('\n'):
     if result.find('modified') != -1:
         prepare_result = result.replace('\tmodified:   ', '')
         print(prepare_result)
-        break
